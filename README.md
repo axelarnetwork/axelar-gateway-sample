2b11d2cfbe6b8e08d61b825e61d704122054f83e
npty <nptytn2@gmail.com>
2022-03-22 19:39:26 +0700

    Add approve and sendToken demo

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Axelar Gateway Demo
+
+This repo demonstrates the usage of `AxelarGateway` in the `@axelar-network/axelarjs-sdk`
+
+## Features
+
+- Approve ERC20 token
+- Send token to the gateway contract
+- Call contract on another chain
+- Call contract on another chain with erc20 token
---
+++ src/index.ts
@@ -0,0 +1,61 @@
+import {
+  EvmChain,
+  AxelarGateway,
+  GatewayTx,
+  Environment,
+} from "@axelar-network/axelarjs-sdk";
+import { ethers } from "ethers";
+
+(async () => {
+  // Initialize the Axelar Gateway
+  const provider = new ethers.providers.JsonRpcProvider("YOUR_RPC_URL"); // Replace this with your rpc url
+
+  // You can create AxelarGateway's instance by two options:
+  // The first option is you know the contract address, so you pass the contract address directly in the constructor function.
+  const gateway = new AxelarGateway("AXELAR_CONTRACT_ADDRESS", provider);
+  // The second option is you don't know the contract address, but you want to use our gateway contract that deployed on mainnet or testnet.
+  const gateway2 = AxelarGateway.create(
+    Environment.TESTNET,
+    EvmChain.ETHEREUM,
+    provider
+  );
+
+  // ----------------------------------------------------------------
+
+  // Approve erc20 tx
+  const unsignedApproveTx = await gateway.createApproveTx({
+    tokenAddress: "YOUR_TOKEN_ADDRESS",
+    amount: "1000000", // Optional, the sdk will approve with max amount (uint256) if not provided.
+  });
+
+  // You can estimate gas before sending
+  const estimatedGasUsed = await unsignedApproveTx.estimateGas();
+  console.log("Estimated gas used for approval:", estimatedGasUsed);
+
+  // Send a transaction
+  const signer = ethers.Wallet.createRandom();
+  const txOption = {
+    maxFeePerGas: "",
+    maxPriorityFeePerGas: "",
+  }; // Optional, ethers.js will handle transaction fee if not specified.
+  const approveReceipt = await unsignedApproveTx.send(signer, txOption);
+
+  // Get transaction hash
+  console.log("Approve transaction hash:", approveReceipt.hash);
+
+  // ----------------------------------------------------------------
+
+  // Send token to gateway contract
+  const unsignedSendTokenTx = await gateway.createSendTokenTx({
+    amount: ethers.utils.parseUnits("1", 18).toString(),
+    destinationAddress: "YOUR_RECEIVER_ADDRESS",
+    destinationChain: EvmChain.AVALANCHE,
+    symbol: "WETH",
+  });
+
+  const sendTokenreceipt = await unsignedSendTokenTx.send(signer);
+
+  console.log("Send token transaction hash:", sendTokenreceipt.hash);
+
+  // -----------------------------------------------------------------
+})();
