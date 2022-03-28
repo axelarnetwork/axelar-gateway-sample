ce9f411825687bd2c6ad741886baadad37700799
npty <nptytn2@gmail.com>
2022-03-28 19:16:58 +0700

    Add avalanche and moonbeam config

--- gmp/hardhat.config.js
+++ gmp/hardhat.config.js
@@ -10,10 +10,19 @@ module.exports = {
       url: process.env.ROPSTEN_RPC,
       accounts: [process.env.PRIVATE_KEY],
     },
+    avalanche: {
+      url: "https://api.avax-test.network/ext/bc/C/rpc",
+      accounts: [process.env.PRIVATE_KEY],
+    },
+    moonbeam: {
+      url: "https://rpc.api.moonbase.moonbeam.network",
+      accounts: [process.env.PRIVATE_KEY],
+    },
   },
   etherscan: {
     apiKey: {
       ropsten: process.env.ETHERSCAN_API_KEY,
+      avalancheFujiTestnet: process.env.SNOWTRACE_API_KEY,
     },
   },
 };
